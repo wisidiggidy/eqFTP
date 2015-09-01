7ffdcf98c1ba6d20a0ce9fb1faac1ffa03bc00b7
Alexander <equals182@gmail.com>
2015-09-02 00:49:19 +0300

    :beer:

--- main.js
+++ main.js
@@ -29,11 +29,99 @@ define(function (require, exports, module) {
     var AppInit = brackets.getModule("utils/AppInit"),
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
         
-        toolbar = require("text!htmlContent/toolbar.html");
+        strings = require("strings"),
+        toolbar = Mustache.render(require("text!htmlContent/toolbar.html"), strings),
+        panel = Mustache.render(require("text!htmlContent/panel.html"), strings),
+        
+        eqftp = {
+            ui: {
+                toolbar: {
+                    toggle: function () {
+                        var panel_element = $("#eqftp__panel");
+                        if (panel_element.length !== 1) {
+                            $(".main-view .content").after(panel);
+                            panel_element = $("#eqftp__panel");
+                        }
+                        if (panel_element.is(":visible")) {
+                            panel_element.hide();
+                            $(".main-view .content").css("right", eqftp.variables.defaults.main_view__content__right);
+                        } else {
+                            var main_toolbar__right = parseInt($("#main-toolbar").css("right").replace("px", ""), 10),
+                                main_toolbar__width = $("#main-toolbar").outerWidth(),
+                                panel__width = 300,
+                                main_view__content__offset = panel__width;
+                            if (main_toolbar__right === 0) {
+                                panel_element.css("right", main_toolbar__width);
+                                eqftp.variables.defaults.main_view__content__right = main_toolbar__width;
+                                main_view__content__offset += main_toolbar__width;
+                            } else {
+                                panel_element.css("right", 0);
+                            }
+                            $(".main-view .content").css("right", main_view__content__offset);
+                            panel_element.width(panel__width).show();
+                        }
+                    }
+                },
+                panel: {
+                    toolbar: {
+                        dropdown: function (params) {
+                            console.log(params);
+                        }
+                    }
+                }
+            },
+            utils: {
+                check: {
+                    isFunction: function (input) {
+                        var getType = {};
+                        return input && getType.toString.call(input) === '[object Function]';
+                    },
+                    isJSON: function (input) {
+                        try { JSON.parse(input); } catch (e) { return false; }
+                        return true;
+                    },
+                    isObject: function (input) {
+                        if (input !== null && typeof input === 'object') { return true; }
+                        return false;
+                    }
+                }
+            },
+            variables: {
+                defaults: {
+                    main_view__content__right: 30
+                }
+            }
+        };
     
     AppInit.htmlReady(function () {
         ExtensionUtils.loadStyleSheet(module, "styles/ext.css");
         $("#main-toolbar .buttons").append(toolbar);
+        
+        $('body').on('click', '[eqftp-click]', function (e) {
+            e.preventDefault();
+            var click = $(this).attr('eqftp-click'),
+                actions = click.split('--'),
+                methods = actions[0].split('__'),
+                tmp = eqftp,
+                args = {};
+            if (!!actions[1]) {
+                var arguments_pairs = actions[1].split(';');
+                arguments_pairs.forEach(function (pair, n) {
+                    pair = pair.split('=');
+                    args[pair[0]] = pair[1];
+                });
+            }
+            methods.some(function (o, n) {
+                if (eqftp.utils.check.isObject(tmp[o])) {
+                    tmp = tmp[o];
+                } else if (eqftp.utils.check.isFunction(tmp[o])) {
+                    tmp[o](args);
+                    return true;
+                } else {
+                    return true;
+                }
+            });
+        });
     });
     
     AppInit.appReady(function () {
